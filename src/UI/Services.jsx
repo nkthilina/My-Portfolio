99a5cbacab8b211adf29246c46d13bde43762bf0
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2023-12-31 13:51:53 +0530

    sixth commit

--- src/UI/Services.jsx
+++ src/UI/Services.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+// import { FaDesktop } from "react-icons/fa";
+import { IoDesktop } from "react-icons/io5";
+import { TbDeviceDesktopSearch, TbDeviceDesktopDown, TbDeviceDesktopStar } from "react-icons/tb";
 
 export default function Services() {
   return (
@@ -23,6 +26,7 @@ export default function Services() {
               {/* left card */}
               <div className="mt-6 sm:mt-0 sm:mb-12">
                 <div className="flex items-center flex-col sm:flex-row">
+
                   <div className="flex justify-start w-full mx-auto items-center">
                     <div className="w-full sm:w-1/2 sm:pr-8">
                       <div
@@ -30,18 +34,124 @@ export default function Services() {
                         data-aos-duration="1200"
                         className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
                       >
-                        <h3></h3>
-                        <p>
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                          Frontend Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7"> 
                           Lorem ipsum dolor sit, amet consectetur adipisicing
-                          elit. Voluptatem, sunt quam asperiores soluta labore
-                          neque!
+                          elit. Voluptatem, sunt quam asperiores
                         </p>
                       </div>
                     </div>
                   </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                    <TbDeviceDesktopSearch className="w-5 h-5"/>
+                    </figure>
+                  </div>
+
                 </div>
               </div>
               {/* end left card */}
+
+              {/* right card */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+
+                  <div className="flex justify-end w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pl-8">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-delay='50'
+                        data-aos-duration="1300"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                          Frontend Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7"> 
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Voluptatem, sunt quam asperiores
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                    <TbDeviceDesktopDown className="w-5 h-5"/>
+                    </figure>
+                  </div>
+
+                </div>
+              </div>
+              {/* end right card */}
+              {/* left card */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+
+                  <div className="flex justify-start w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pr-8">
+                      <div
+                        data-aos="fade-right"
+                        data-aos-delay='100'
+                        data-aos-duration="1400"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                          Full Stack Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7"> 
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Voluptatem, sunt quam asperiores
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                    <TbDeviceDesktopStar className="w-5 h-5"/>
+                    </figure>
+                  </div>
+
+                </div>
+              </div>
+              {/* end left card */}
+
+              {/* right card */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+
+                  <div className="flex justify-end w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pl-8">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-duration="1500"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                          Frontend Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7"> 
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Voluptatem, sunt quam asperiores
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                    <IoDesktop className="w-5 h-5"/>
+                    </figure>
+                  </div>
+
+                </div>
+              </div>
+              {/* end right card */}
+
             </div>
           </div>
         </div>
