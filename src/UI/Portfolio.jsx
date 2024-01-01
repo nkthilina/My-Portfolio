80b12b4cce55c63203b766d68c193f9a5d9cf935
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-01-01 13:32:14 +0530

    ninth commit

--- src/UI/Portfolio.jsx
+++ src/UI/Portfolio.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import data from "../assets/data/portfolioData";
+import Modal from "./Modal"
 
 export default function Portfolio() {
-  const [nextItems, setNextItems] = useState(6);
-  const [portfolios, setPortfolios] = useState(data);
-  const [selectTab, setSelectTab] = useState("all");
+  const [nextItems, setNextItems] = useState(6)
+  const [portfolios, setPortfolios] = useState(data)
+  const [selectTab, setSelectTab] = useState("all")
+  const [showModal, setShowModal] = useState(false)
+  const [activeId, setActiveId] = useState(null)
 
   const loadMoreHandler = () => {
     setNextItems((prev) => prev + 3);
+  };
+
+  const showModalHandler = (id) => {
+    setShowModal(true)
+    setActiveId(id)
   };
 
   useEffect(() => {
@@ -71,7 +79,7 @@ export default function Portfolio() {
               </figure>
               <div className="w-full h-full bg:primaryColor bg:opacity-40 absolute top-0 left-0 z-5 hidden group-hover:block">
                 <div className="w-full h-full flex items-center justify-center">
-                  <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200">
+                  <button onClick={() => showModalHandler(portfolio.id)} className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200">
                     See details
                   </button>
                 </div>
@@ -84,13 +92,16 @@ export default function Portfolio() {
           {nextItems < portfolios.length && data.length > 6 && (
             <button
               onClick={loadMoreHandler}
-              className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+              className="text-white bg-primaryColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
             >
               Load more
             </button>
           )}
         </div>
       </div>
+      {
+        showModal && <Modal setShowModal={setShowModal} activeID={activeId}/>
+      }
     </section>
   );
 }
